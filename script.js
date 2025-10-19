9c5788f40a64512631bada273c356547ad9db376
Ven <97375170+venjoverse@users.noreply.github.com>
2025-10-19 12:35:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const navLinks = document.querySelector('.nav-links');
 
-  hamburger.addEventListener('click', () => {
+    hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navLinks.classList.toggle('open');
   });
